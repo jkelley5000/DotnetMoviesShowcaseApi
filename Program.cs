910aa8ac24b0bc7371746b470a98e81727381504
Joseph Kelley <josephkelley91@gmail.com>
2024-10-28 21:01:15 -0400

    Added Swagger documentation to the project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo {
+         Title = "Movies Showcase API",
+         Description = "Swagger documentation for the Movies Showcase API project",
+         Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "DotnetMoviesShowcaseApi API V1");
+   });
+}
 
 app.MapGet("/", () => "Hello World!");
 
