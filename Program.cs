87c76e66e380f7ece3fadc1971da3dbf4f6f946d
Joseph Kelley <josephkelley91@gmail.com>
2024-12-01 09:36:00 -0500

    Re-configures methods invoked when different URIs are called, to be the extended methods defined and overridden in the MoviesShowsDbOperations class.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using DotnetMoviesShowcaseApi.DB;
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile(@"C:\Users\da-at\OneDrive\Documents\GitHub\connections\DotnetMoviesShowcaseApiConnectionString.json", optional: false, reloadOnChange: true);
 var connectionString = builder.Configuration.GetConnectionString("MyConnection");
-builder.Services.AddSingleton(new MoviesShowcaseDB(connectionString));
+builder.Services.AddSingleton<MoviesShowsDbOperations>(provider => new MoviesShowsDbOperations(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -38,24 +38,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("AllowSpecificOrigin");
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/all-movies-shows", () => MoviesShowcaseDB.GetMoviesShows(connectionString));
-app.MapGet("/all-movies", () => MoviesShowcaseDB.GetMovies(connectionString));
-app.MapGet("/all-shows", () => MoviesShowcaseDB.GetShows(connectionString));
-app.MapGet("/movies-by-genre", (string? genre) => 
+app.MapGet("/all-movies-shows", (MoviesShowsDbOperations dbOps) => dbOps.GetMoviesShows());
+app.MapGet("/all-movies", (MoviesShowsDbOperations dbOps) => dbOps.GetMovies());
+app.MapGet("/all-shows", (MoviesShowsDbOperations dbOps) => dbOps.GetShows());
+app.MapGet("/movies-by-genre", (string? genre, MoviesShowsDbOperations dbOps) => 
 {
    if (string.IsNullOrEmpty(genre))
    {
-      return MoviesShowcaseDB.GetMovies(connectionString);
+      return dbOps.GetMovies();
    }
-   return MoviesShowcaseDB.GetMoviesByGenre(connectionString, genre);
+   return dbOps.GetMoviesByGenre(genre);
 });
-app.MapGet("/shows-by-genre", (string? genre) => 
+app.MapGet("/shows-by-genre", (string? genre, MoviesShowsDbOperations dbOps) => 
 {
    if (string.IsNullOrEmpty(genre))
    {
-      return MoviesShowcaseDB.GetShows(connectionString);
+      return dbOps.GetShows();
    }
-   return MoviesShowcaseDB.GetShowsByGenre(connectionString, genre);
+   return dbOps.GetShowsByGenre(genre);
 });
 
 app.Run();
